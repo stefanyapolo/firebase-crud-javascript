ccd828861dd7415ef62278b81cc94c92993ffe71
stefanyapolo <stefany.apolo@garajedeideas.com>
2022-05-12 18:33:48 +0200

    starting delete button

--- firebase.js
+++ firebase.js
@@ -5,6 +5,10 @@ import {
   collection,
   addDoc,
   getDocs,
+  onSnapshot,
+  deleteDoc,
+  doc
+  
 } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,10 +28,20 @@ const app = initializeApp(firebaseConfig);
 //el objeto db es la conexión a la bbdd
 const db = getFirestore();
 
-export const saveTask = (title, description) =>
+const saveTask = (title, description) =>
   //añado un documento a la collection que usa la conexion de la db
   addDoc(collection(db, "tasks"), { title, description });
 
-export const getTasks = () => {
-  getDocs(collection(db, 'tasks'));
-};
+const getTasks = () => 
+  getDocs(collection(db, "tasks"));
+
+
+
+const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
+
+ /*  solo voy a eliminar una tarea así que le paso doc, la conexion de la bd, la collection
+  en la que esta y el id para eliminar */
+const deleteTask=(id)=>deleteDoc(doc(db,"tasks", id));
+
+export{saveTask, getTasks, onGetTasks, onSnapshot, collection, db, deleteTask};
--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-import { saveTask, getTasks } from './firebase.js';
+import { saveTask, getTasks, onSnapshot, collection, db, deleteTask } from "./firebase.js";
 
-//evento que permite ejecutar cuando la pagina cargue
-window.addEventListener('DOMContentLoaded', async () => {
-  const querySnapshot = await getTasks();
-  querySnapshot.forEach(doc=>{
-      console.log(doc);
-  })
+const taskForm = document.getElementById("task-form");
+const tasksContainer = document.getElementById("tasks-container");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  /* onSnapshot está escuchando y cuando ocurra un cambio en la db tasks, 
+  recibo los datos nuevos en querySnapshot */
+  const querySnapshot=await getTasks();
+
+    console.log(querySnapshot);
+    let html = "";
+    //recorro los datos y relleno el html
+    querySnapshot.forEach((doc) => {
+      const task = doc.data();
+      html += `
+        <div>
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+        <button class='btnDelete' data-id="${doc.id}">Delete</button>
+        </div>
+        `;
+    });
+
+    tasksContainer.innerHTML = html;
+    const btnsDelete= tasksContainer.querySelectorAll('.btnDelete');
+    btnsDelete.forEach((btn) =>{
+      //extraigo del evento event el target y de este el dataset
+      btn.addEventListener('click', ({target: {dataset}}) =>{
+        deleteTask(dataset.id)
+        
+      })
+    })
+ 
 });
-const taskForm = document.getElementById('task-form');
-taskForm.addEventListener('submit', (e) => {
+
+taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   //traigo de taskForm los input
-  const title = taskForm['task-title'];
-  const description = taskForm['task-description'];
+  const title = taskForm["task-title"];
+  const description = taskForm["task-description"];
 
   saveTask(title.value, description.value);
 
